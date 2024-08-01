3c3daef06795a94c99912d3303bb98253fa4d10b
jjiunnbeh <jjiunnbeh@gmail.com>
2024-08-01 21:27:34 +0800

    add loading spinner

--- lib/pages/forgetPassword.dart
+++ lib/pages/forgetPassword.dart
@@ -5,6 +5,7 @@ import 'package:mobile_chat_app/components/input.dart';
 import 'package:mobile_chat_app/components/mybutton.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class ForgetPassword extends StatefulWidget {
   const ForgetPassword({super.key});
@@ -17,6 +18,7 @@ class ForgetPassword extends StatefulWidget {
 class _ForgetPasswordState extends State<ForgetPassword> {
   String? email;
   String? alert;
+  bool showLoading = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,72 +26,78 @@ class _ForgetPasswordState extends State<ForgetPassword> {
         title: const Text('Forget Password', style: kAppBarTextStyle),
         centerTitle: true,
       ),
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Center(
-                    child: HeroLogo(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 100.0,
-                        ),
-                        margin:
-                            const EdgeInsets.only(left: 100.0, right: 100.0)),
+      body: ModalProgressHUD(
+        inAsyncCall: showLoading,
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: HeroLogo(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 100.0,
+                          ),
+                          margin:
+                              const EdgeInsets.only(left: 100.0, right: 100.0)),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Input(
-                    onChanged: (value) {
-                      setState(() {
-                        email = value;
-                      });
-                    },
-                    keyboard: kEmailKeyboard,
-                    hint: 'Email',
-                  ),
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                  MyButton(
-                    margin: const EdgeInsets.only(
-                        top: 10.0, left: 20.0, right: 20.0),
-                    text: 'Send Reset Email',
-                    onPressed: () async {
-                      try {
-                        await Firebase.initializeApp();
-                        await FirebaseAuth.instance
-                            .sendPasswordResetEmail(email: email.toString());
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Input(
+                      onChanged: (value) {
                         setState(() {
-                          alert =
-                              'A password reset link has been sent to your registered email address.';
+                          email = value;
                         });
-                      } catch (error) {
-                        print('Error starts here:');
-                        print(error);
-                        print("Error ends here.");
-                      }
-                    },
-                  ),
-                  alert != null
-                      ? Text(
-                          alert.toString(),
-                          style: kAlertTextStyle,
-                          textAlign: TextAlign.center,
-                        )
-                      : const SizedBox(width: 0, height: 0,),
-                ],
+                      },
+                      keyboard: kEmailKeyboard,
+                      hint: 'Email',
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    MyButton(
+                      margin: const EdgeInsets.only(
+                          top: 10.0, left: 20.0, right: 20.0),
+                      text: 'Send Reset Email',
+                      onPressed: () async {
+                        setState(() {
+                          showLoading = true;
+                        });
+                        try {
+                          await Firebase.initializeApp();
+                          await FirebaseAuth.instance
+                              .sendPasswordResetEmail(email: email.toString());
+                          setState(() {
+                            showLoading = false;
+                            alert = 'A password reset link has been sent to your registered email address.';
+                          });
+                        } catch (error) {
+                          print('Error starts here:');
+                          print(error);
+                          print("Error ends here.");
+                        }
+                      },
+                    ),
+                    alert != null
+                        ? Text(
+                            alert.toString(),
+                            style: kAlertTextStyle,
+                            textAlign: TextAlign.center,
+                          )
+                        : const SizedBox(width: 0, height: 0,),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
